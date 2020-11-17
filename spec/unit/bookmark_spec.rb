04f2b48d3ec54e93902ff4cc133145f4cc5f7d93
CodeZeus-dev <constantine.akritides@gmail.com>
2020-11-17 15:08:21 +0000

    added a unit test to check that upon initialisation, a url list is created

--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -7,4 +7,10 @@ describe Bookmark do
   it 'creates a Bookmark instance' do
     expect(bookmark).to be_instance_of(described_class)
   end
+
+  context "#initialize" do
+    it "creates a list of urls" do
+      expect(bookmark.url_list.is_a?(Array)).to eq(true)
+    end
+  end
 end
